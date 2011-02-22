7398a7e15d3abc147757339e3679e903199f6a37
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-02-22 10:29:43 +0900

    add Elasticfox_app

---
+++ Elasticfox_app/Contents/Resources/defaults/preferences/prefs.js
@@ -0,0 +1,10 @@
+pref("toolkit.defaultChromeURI", "chrome://ec2ui/content/ec2ui_main_window.xul");
+pref("signon.rememberSignons", true);
+pref("signon.expireMasterPassword", false);
+pref("signon.SignonFileName", "signons.txt");
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
