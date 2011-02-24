b3ed88dc64874027a514b7e60060fcaaefbcac1a
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-02-24 12:59:38 +0900

    add build app script

---
+++ build-app.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cp -pr Elasticfox_app Elasticfox.app
+cp -pr ec2ui/ Elasticfox.app/Contents/Resources/chrome/ec2ui
+mv Elasticfox.app/Contents/Resources/ec2ui_main_window.xul Elasticfox.app/Contents/Resources/chrome/ec2ui/content/ec2ui/
+cd Elasticfox.app/Contents/Resources/chrome/ec2ui
+zip -r ec2ui.jar .
+mv ec2ui.jar ..
+cd ..
+rm -rf ec2ui/
+cd ../..
+mkdir Frameworks
+rsync -rl /Library/Frameworks/XUL.framework Frameworks/
