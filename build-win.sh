f83f1a331480ec0223e51138a33e83931eb50b5a
winebarrel <sgwr_dts@yahoo.co.jp>
2011-02-28 22:42:53 +0900

    etc

--- build-win.sh
+++ build-win.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -rf elasticfox-win
+rm -rf elasticfox-win *setup.exe
 cp -pr Elasticfox_app/Contents/Resources elasticfox-win
 cp -pr ec2ui/ elasticfox-win/chrome/ec2ui
 mv elasticfox-win/ec2ui_main_window.xul elasticfox-win/chrome/ec2ui/content/ec2ui/
@@ -10,4 +10,6 @@ cd ..
 rm -rf ec2ui/
 cd ..
 rsync -rl "/cygdrive/c/Program Files/xulrunner" ./
- 
+cd ../
+cygstart -w elasticfox.ci
+mv setup.exe Elasticfox-ec2tag-0.2.4-setup.exe
