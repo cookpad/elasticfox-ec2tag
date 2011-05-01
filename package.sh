a1439f9f78732efb12be71eec414cfa42854e46a
winebarrel <sgwr_dts@yahoo.co.jp>
2011-05-01 13:32:12 +0900

    append version tpo xpi filename

--- package.sh
+++ package.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+VERSION=0.2.6
 rm -f *.xpi
 cd ec2ui/
 zip -r ../elasticfox/chrome/ec2ui.jar .
 cd ../elasticfox/
-zip -r ../elasticfox-ec2tag.xpi .
+zip -r ../elasticfox-ec2tag-${VERSION}.xpi .
 rm -f chrome/ec2ui.jar
