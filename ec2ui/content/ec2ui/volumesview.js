f0205a342fae89d52377f9aed78a462b238facd8
winebarrel <sgwr_dts@yahoo.co.jp>
2011-06-26 22:29:32 +0900

    add ec2tag when create volumen

--- ec2ui/content/ec2ui/volumesview.js
+++ ec2ui/content/ec2ui/volumesview.js
@@ -138,6 +138,7 @@ var ec2ui_VolumeTreeView = {
                 if (vol) {
                     vol.tag = retVal.tag;
                     ec2ui_session.setResourceTag(vol.id, vol.tag);
+                    __tagging2ec2__([vol.id], ec2ui_session, vol.tag);
                 }
             }
         }
