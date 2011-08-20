35ba896bb675f818fb0e0d919fd4028b8b8d8758
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-08-20 19:57:23 +0900

    add /dev/sda1 filter

--- ec2ui/content/ec2ui/volumesview.js
+++ ec2ui/content/ec2ui/volumesview.js
@@ -36,7 +36,24 @@ var ec2ui_VolumeTreeView = {
 
     invalidate : function() {
         var target = ec2ui_VolumeTreeView;
-        target.displayImages(target.filterImages(ec2ui_model.volumes));
+        var list = (ec2ui_model.volumes || []);
+        var filterRootDev = document.getElementById("ec2ui.volumes.norootdev").checked;
+
+        if (filterRootDev) {
+          var newList = [];
+
+          for (var i = 0; i < list.length; i++) {
+            var volume = list[i];
+
+            if (volume.device != '/dev/sda1') {
+              newList.push(volume);
+            }
+          }
+
+          list = newList;
+        }
+
+        target.displayImages(target.filterImages(list));
     },
 
     searchChanged : function(event) {
