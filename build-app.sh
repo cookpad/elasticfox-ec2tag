5a3a5b8e988c31f89979cb1c34b1990ee2b82ab1
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-10-29 15:54:27 +0900

    etc

--- build-app.sh
+++ build-app.sh
@@ -4,7 +4,7 @@ cp -pr Elasticfox_app Elasticfox.app
 cp -pr ec2ui/ Elasticfox.app/Contents/Resources/chrome/ec2ui
 mv Elasticfox.app/Contents/Resources/ec2ui_main_window.xul Elasticfox.app/Contents/Resources/chrome/ec2ui/content/ec2ui/
 cd Elasticfox.app/Contents/Resources/chrome/ec2ui
-zip -r ec2ui.jar .
+zip -qr ec2ui.jar .
 mv ec2ui.jar ..
 cd ..
 rm -rf ec2ui/
