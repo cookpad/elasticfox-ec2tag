6482102c8b5f2e154ea984487e56457c75720390
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2012-03-25 22:49:20 +0900

    change mode

--- Elasticfox_app/Contents/Resources/defaults/preferences/prefs.js
+++ Elasticfox_app/Contents/Resources/defaults/preferences/prefs.js
@@ -3,10 +3,8 @@ pref("signon.rememberSignons", true);
 pref("signon.expireMasterPassword", false);
 pref("signon.SignonFileName", "signons.txt");
 /* debugging prefs */
-/*
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
-*/
